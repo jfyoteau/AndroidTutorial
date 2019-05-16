8cfe3539125b38b7a02e9db81c3426414f1d11c0
Jean-Francois Yoteau <jfyoteau@yahoo.fr>
2019-05-16 10:06:19 +0900

    Set the design of screen 1 and add the action to the others screens

--- app/src/main/java/io/github/jfyoteau/androidtutorial/fragment/Screen1Fragment.kt
+++ app/src/main/java/io/github/jfyoteau/androidtutorial/fragment/Screen1Fragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import io.github.jfyoteau.androidtutorial.MyFragmentActivity
 import io.github.jfyoteau.androidtutorial.R
 
 class Screen1Fragment : Fragment() {
@@ -14,5 +16,23 @@ class Screen1Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? =
         inflater.inflate(R.layout.fragment_screen1, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.screen1_button)?.apply {
+            setOnClickListener {
+                val activity = this@Screen1Fragment.activity as? MyFragmentActivity ?: return@setOnClickListener
+                activity.presentFragment(Screen1Fragment())
+            }
+        }
+
+        view.findViewById<Button>(R.id.screen2_button)?.apply {
+            setOnClickListener {
+                val activity = this@Screen1Fragment.activity as? MyFragmentActivity ?: return@setOnClickListener
+                activity.presentFragment(Screen2Fragment())
+            }
+        }
+    }
 
 }
